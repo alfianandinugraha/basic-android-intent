9050da58351a533676bbce43db586d2ca9fb58ff
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-25 10:28:42 +0700

    feat: add message and new activity

--- app/src/main/java/com/example/androidintent/MainActivity.kt
+++ app/src/main/java/com/example/androidintent/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.androidintent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,26 @@ class MainActivity : AppCompatActivity() {
 
 
         val buttonToRegister = findViewById<Button>(R.id.btn_to_register)
+        val buttonLogin = findViewById<Button>(R.id.btn_login)
+        val inputEmailView = findViewById<EditText>(R.id.input_email)
+        val inputPasswordView = findViewById<EditText>(R.id.input_password)
+
+        buttonLogin.setOnClickListener {
+            val emailInput = inputEmailView.text.toString();
+            val passwordInput = inputPasswordView.text.toString();
+
+            val requireEmail = "responsi@pemrogramanmobile.com";
+            val requirePassword = "mypassword123"
+
+            if (emailInput != requireEmail || passwordInput != requirePassword) {
+                Toast.makeText(this, "username atau password salah", Toast.LENGTH_LONG).show()
+            }
+
+            Intent(this, HomeActivity::class.java)
+                .also {
+                    startActivity(it)
+                }
+        }
 
         buttonToRegister.setOnClickListener {
             Intent(this, RegisterActivity::class.java)
